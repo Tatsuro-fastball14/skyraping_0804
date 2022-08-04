fc619128eaf51d29991f32cab211bf148f4c5075
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-04 22:57:37 +0900

    a
    a

--- scraping.py
+++ scraping.py
@@ -1,5 +1,17 @@
 from email.mime import base
 
 
+def main():
+  base_url = 'https://finance.yahoo.com/quote/'
+
+  stocks =['AAPL','MSFT','GME','GOOG','T']
+
+  res = req.get(base_url)
+
+
+  print(f'[-] Status code: {res.status_code}')
+
+  return
+
 
 
