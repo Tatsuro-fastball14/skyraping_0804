5b330d6b0d97eb67f9f292fc64f611cf6a5340f0
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-06 07:18:24 +0900

    udemy
    udemy

--- scraping.py
+++ scraping.py
@@ -11,7 +11,30 @@ def main():
 
   print(f'[-] Status code: {res.status_code}')
 
-  return
+  for ticker in stocks:
+      stockpage = req.get(base_usl+ticker)
+      print(f'[-] Status code: {stockpage.status_code}')
+      if stockpage.status_code ==200:
+
+        source = stockpage.text
+        parsedpage = soup(source, 'html.parser')
+        try:
+          price = parsedpage.find('span', class = 'Trdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
+          print(f'[-] Ticker {ticker} has price {price}!')
+          print(f'/t[-] {base_url+ticker}')
+
+
+          try:
+            try:
+                delta = parsedpage.find('span', attrs={'data-reactid:50'}).text
+
+
+
+
+      else:
+
+        print(f'[x] Request to page {base_url+ticker} failed, status code: {stockpage.status_code}')
+        time.sleep(1)
 
 
 
