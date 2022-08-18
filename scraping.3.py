cbe311a89a1fbfe5dbcb437f673838e688a93d6c
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-19 07:08:59 +0900

    修正
    修正

--- scraping.3.py
+++ scraping.3.py
@@ -2,10 +2,27 @@ import sys, requests, hashlib, time
 from bs4 import BeautifulSoup as bs
 import psycopg2 as psql
 
+def connectdb(ip='', dbname='', username='', password=''):
 
+
+    return
+
+def pullpage(pagelink=''):
+
+    return
+
+def insertdb(obj={}, conn='', cur=''):
+
+  return
+
+def checkdb(obj, conn, cur):
+
+  return
 
 def main():
 
+  try:
+      rootnode = svs.argv[1]
   return
 
 if __name__ ""  '_main_':
