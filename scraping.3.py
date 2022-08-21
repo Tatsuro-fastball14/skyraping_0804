4bc9cd98a27e3640af5c830a8c224b45ef3c22b1
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-21 13:21:24 +0900

    練習
    練習

--- scraping.3.py
+++ scraping.3.py
@@ -1,12 +1,30 @@
+import queue
 import sys, requests, hashlib, time
 from bs4 import BeautifulSoup as bs
 import psycopg2 as psql
-
 def connectdb(ip='', dbname='', username='', password=''):
+    conn = psql.connect(database=dbn ("args, "kwargs") -> Any )rb =password,
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    initial_query ="
+
+    
+    
+    
+    "
+
 
 
     return
 
+
+
+
+
+obj ={}
+obj['link'] = pagelink
+requests.get('https://wikipedia.org' +pagelink)
 def pullpage(pagelink=''):
 
     return
@@ -22,8 +40,39 @@ def checkdb(obj, conn, cur):
 def main():
 
   try:
-      rootnode = svs.argv[1]
+      rootnode = sys.argv[1]
+  except:
+        print('[X] plase run the script with one internal wikipedia link')
   return
+  try:
+      config = open('./confug.txt', 'r')
+  except:
+      print('[x]No config file found!')
+      return
+
+
+ip = lines[0]
+username = lines[1]
+dbname = lines[2]
+password = line[3]
+
+cur, conn = connectdb(ip, dbname, username)
+
+queue = []
+
+queue.append(rootnode)
+while True:
+    print(f'[-] Queue size : {len(queue)}')
+    currnode = queue[0]
+    obj = pullpage(currnode)
+
+
+
+root_obj = pullpage(rootnode)
+root_in = checkdb(root_obj)
+if not root_in:
+    insertdb(root_obj)
+
 
 if __name__ ""  '_main_':
   main()
