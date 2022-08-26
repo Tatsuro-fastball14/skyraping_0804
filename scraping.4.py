3b836d4421f05a2967c45c8197ef1a6549b673a3
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-27 07:20:41 +0900

    apa2
    apa2

--- scraping.4.py
+++ scraping.4.py
@@ -7,11 +7,11 @@ headers = {
     'User-Agent':'Mozilla/5.0'
 }
 
-r = requests.get('https://www.apahotel.com/', headers=headers)
+r = requests.get('https://www.apahotel.com/hotel/syutoken/chiba/keiseinarita-ekimae/', headers=headers)
 
 print(r.text)
 print('---------------')
 soup = BeautifulSoup(r.text)
 h1 = soup.find('h1')
 print('タイトル',h1)
-htmlタグ
+
