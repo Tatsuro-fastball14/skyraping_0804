2db723d51244c65636e1b4401a34de807fff4095
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-10 03:06:00 +0900

    このサイトのトップページから最新記事を取得して、記事が更新されているかどうかを判定する」プログラムを作ってみます。
    このサイトのトップページから最新記事を取得して、記事が更新されているかどうかを判定する」プログラムを作ってみます。

--- scraping.4.py
+++ scraping.4.py
@@ -1,6 +1,7 @@
 #アパホテルのサイトをスクレイピングするコード
 
 import requests
+import pickle
 from bs4 import BeautifulSoup
 
 headers = {
@@ -15,3 +16,16 @@ soup = BeautifulSoup(r.content, 'html.parser')
 title = soup.find('title')
 print('タイトル',title)
 soup.find_all('div',"class=t-shigiraGuestroomsDetail__roomPlanArea")
+
+with open('./before_article.pickle', 'rb')as f:
+    before_new_article =pickle.load(f)
+
+html =requests.get(r)
+soup =BeautifulSoup(html.content, "html.parser")
+
+new_article = str(soup.find(class_="entry-card-wrap").get("title"))
+with open('./before_article.pickle', 'wb') as f:
+    pickle.dump(before_article.pickle, f)
+
+if new_article != before_new_article:
+    print('新着あり')
